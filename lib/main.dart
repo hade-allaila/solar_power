942087c62eb1e47fe139fe586a9b0b7341393b71
hade-allaila <hade.allaila@gmail.com>
2025-08-15 18:45:27 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:solar_app/networking/api_client.dart';
 import 'package:solar_app/solar_power_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   var prefs = await SharedPreferences.getInstance();
   String? ipAddress =  prefs.getString('ipaddress');
   ApiClient.instance.configureDio(ipAddress);
